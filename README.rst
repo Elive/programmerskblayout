794da54e8047998e546bea6fbe3f4950c6a5fbbd
Thanatermesis <thanatermesis@gmail.com>
2013-01-06 17:43:34 +0100

    version 7, which supports any language/mapping

--- README.rst
+++ README.rst
@@ -7,12 +7,14 @@ Thanatermesis keyboard layout
 Introduction
 ==============
 
-This is a simple own modification of the keyboard layout (spanish variant) that fits better my needs, which focuses in different features:
- 
+This is a simple own modification of the keyboard layout that fits better my needs, which focuses in different features:
+
  * include a better combination of keys more fast to type for common programming languages
  * better access to common programming languages instead to fail it and lose time pressing the delete key
  * it doesn't change your default layout, so you don't need to learn a new layout (only since the version 6)
  * very easy to remember the location of the new characters
+
+**Update**: Since version 7 it actually supports any distribution of keyboard (not only spanish)
 
 
 How the distribution is made
--- switch.sh
+++ switch.sh
@@ -1,12 +1,4 @@
-#!/bin/bash 
-source $HOME/Git/git/share/tools/functions
-make_environment
-
-# Usage
-if [[ -z "${1}" ]] ; then
-   echo -e "Usage: $(basename $BASH_SOURCE) d (default)  v5 (version 5)"
-   exit 1
-fi 
+#!/bin/bash
 
 main(){
    # pre {{{
@@ -15,18 +7,7 @@ main(){
    # }}}
 
 
-   case $1 in
-      d|default)
-         setxkbmap es
-         xmodmap $HOME/.Xmodmap.hhkb.Zeus
-         ;;
-      v*)
-         version="${1#v}"
-         setxkbmap es
-         xmodmap $HOME/.Xmodmap.hhkb.Zeus
-         xmodmap xmodmap_v${version}.0rc1.kbmap
-      ;;
-   esac
+   xmodmap "$( ls -1 xmodmap_* | sort -g | tail -1 )"
 
    sleep 1
    echo -e "Please drag the first button of the mouse (selecting text) around some seconds until the selection works (keyboard fully loaded correctly)"
@@ -39,4 +20,4 @@ main(){
 #
 main "$@"
 
-# vim: set foldmethod=marker : 
+# vim: set foldmethod=marker :
