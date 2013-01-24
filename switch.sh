ab5d699abadb1ca0ac39792b4e677e6398360bc4
Thanatermesis <thanatermesis@gmail.com>
2013-01-24 22:12:15 +0100

    misc improvings

--- switch.sh
+++ switch.sh
@@ -7,7 +7,13 @@ main(){
    # }}}
 
 
+   echo -e "Please wait..." # by some reason this is slow on e17
+   #sleep 1
+   source /etc/default/keyboard
+   setxkbmap "$XKBLAYOUT"
    xmodmap "$( ls -1 xmodmap_* | sort -g | tail -1 )"
+   #sync
+   echo -e "Done (or almost)"
 
    sleep 1
    echo -e "Please drag the first button of the mouse (selecting text) around some seconds until the selection works (keyboard fully loaded correctly)"
