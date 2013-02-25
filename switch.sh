faf9e29f4715614dac28bc2cb0f8ecdabb90d6fb
Thanatermesis <thanatermesis@gmail.com>
2013-02-25 17:30:45 +0100

    fixed a bug with US keyboards where altgr is not enabled by default

--- switch.sh
+++ switch.sh
@@ -10,13 +10,24 @@ main(){
    echo -e "Please wait..." # by some reason this is slow on e17
    #sleep 1
    source /etc/default/keyboard
-   setxkbmap "$XKBLAYOUT"
+   case $XKBLAYOUT in
+       es)
+           setxkbmap "$XKBLAYOUT"
+           ;;
+       us|*)
+           # some keyboards like US one needs to enable the "altgr" option specifically
+           setxkbmap -variant altgr-intl "$XKBLAYOUT"
+   esac
+
+   # set our special kb settings
    xmodmap "$( ls -1 xmodmap_* | sort -g | tail -1 )"
+
    #sync
-   echo -e "Done (or almost)"
+   #echo -e "Done (or almost)"
 
    sleep 1
-   echo -e "Please drag the first button of the mouse (selecting text) around some seconds until the selection works (keyboard fully loaded correctly)"
+   #echo -e "Please drag the first button of the mouse (selecting text) around some seconds until the selection works (keyboard fully loaded correctly)"
+   echo -e "Done, keyboard layout updated"
 
 
 }
